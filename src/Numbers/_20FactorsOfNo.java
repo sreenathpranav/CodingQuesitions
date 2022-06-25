43d8da62b523b727540b5f02dec3d4787a31c3a0
S Pranav <sreenathpranav1@gmail.com>
2022-06-26 03:12:14 +0530

    day 7

--- src/Numbers/_20FactorsOfNo.java
+++ src/Numbers/_20FactorsOfNo.java
@@ -2,6 +2,9 @@ package Numbers;
 
 public class _20FactorsOfNo {
     public static void main(String[] args) {
+/*Time Complexity: O(N)
+Space Complexity: O(1)*/
+
         int n1=9,n2=14;
         for(int i=1;i<=n1;i++){
             if(n1%i==0){
@@ -11,13 +14,17 @@ public class _20FactorsOfNo {
         System.out.println();
         optimisedSolution(n2);
     }
+
+
+    /*Time Complexity: O(sqrt(N))
+     Space Complexity: O(1)*/
     static void optimisedSolution(int n){
 
         for(int i=1;i<Math.sqrt(n);i++){
             if(n%i==0){
 
                 //if divisors are equal,print only one
-                if(n/i==1)
+                if(n/i==i)
                     System.out.print(" "+i);
 
                 else //or print both
